0c1bdce717287466057ee6fb834064f951c9973f
raylivesun <162396999+raylivesun@users.noreply.github.com>
2025-05-03 22:38:12 -0300

    the main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <math.h>
+#include <string>
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << endl;
+    return 0;
+}
